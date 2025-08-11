de5f4cb5077088920eb48958a0b9181d91e1f479
Mikaela Purugganan <mikaela.programming@gmail.com>
2025-08-11 17:44:04 +0800

    feat(signup): integrate Axios and Toaster for API calls and success/error notifications

--- frontend/components/Auth/Signup.tsx
+++ frontend/components/Auth/Signup.tsx
@@ -1,17 +1,71 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import PasswordInput from "./PasswordInput";
+import LoadingButton from "../Helper/LoadingButton";
+import Link from "next/link";
+import { BASE_API_URL } from "@/server";
+import axios from "axios";
+import { handleAuthRequest } from "../utils/apiRequest";
+import { toast } from "sonner";
+
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 const Signup = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    // create request
+    const signupReq = async () =>
+      await axios.post(`${BASE_API_URL}/users/signup`, formData, {
+        withCredentials: true,
+      });
+    const result = await handleAuthRequest(signupReq, setIsLoading);
+
+    if (result) {
+      console.log(result.data.data.user);
+      toast.success(result.data.message);
+    }
+
+    // TODO :
+    // 1. Redirect to Homepage
+    // 2. Add user to redux store
+  };
+
   return (
-    <div className="relative w-full h-screen bg-cover bg-[url('/images/Banner.jpg')]">
-      <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] bg-white rounded-2xl w-75 h-fit p-6 flex flex-col items-center">
-        <h1 className="text-xl font-bold sm:text-2xl text-center">
+    <div className="relative w-full h-screen bg-cover bg-center bg-[url('/images/Banner.jpg')]">
+      <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] bg-white rounded-2xl w-100 md:w-125 lg:w-125 xl:w-150 h-fit p-6 flex flex-col items-center">
+        <h1 className="text-xl font-bold sm:text-2xl lg:text-3xl text-center">
           Sign up with <span className="text-blue-600">Photogram</span>
         </h1>
-        <form className="w-[90%] mt-4">
+        <form
+          className="w-[90%] mt-4"
+          onSubmit={handleSubmit}
+        >
           <div className="mb-4">
-            <label htmlFor="name" className="font-semibold mb-2 block">
+            <label
+              htmlFor="name"
+              className="font-semibold mb-2 block"
+            >
               Username
             </label>
             <input
@@ -20,10 +74,15 @@ const Signup = () => {
               placeholder="Username"
               id="name"
               className="px-4 py-2 bg-gray-200 rounded-lg w-full block outline-none"
+              value={formData.username}
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="font-semibold mb-2 block">
+            <label
+              htmlFor="email"
+              className="font-semibold mb-2 block"
+            >
               Email
             </label>
             <input
@@ -32,6 +91,8 @@ const Signup = () => {
               placeholder="Email address"
               id="email"
               className="px-4 py-2 bg-gray-200 rounded-lg w-full block outline-none"
+              value={formData.email}
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -39,15 +100,35 @@ const Signup = () => {
               label="Password"
               name="password"
               placeholder="Enter password"
+              value={formData.password}
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
             <PasswordInput
-              name="passConfirm"
+              name="passwordConfirm"
               label="Confirm Password"
               placeholder="Confirm Password"
+              value={formData.passwordConfirm}
+              onChange={handleChange}
             />
           </div>
+          <LoadingButton
+            size={"lg"}
+            className="w-full mt-3"
+            type="submit"
+            isLoading={isLoading}
+          >
+            Sign Up
+          </LoadingButton>
+          <h2 className="mt-4 text-md text-center text-gray-800">
+            Already have an account?
+            <Link href="auth/login">
+              <span className="text-blue-800 underline cursor-pointer font-medium">
+                Login here
+              </span>
+            </Link>
+          </h2>
         </form>
       </div>
     </div>
---
+++ frontend/components/utils/apiRequest.ts
@@ -0,0 +1,30 @@
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+
+interface ApiErrorResponse {
+  message: string;
+}
+
+export const handleAuthRequest = async <T>(
+  requestCallBack: () => Promise<T>,
+  setLoading?: (loading: boolean) => void
+): Promise<T | null> => {
+  if (setLoading) setLoading(true);
+  try {
+    const response = await requestCallBack();
+    return response;
+  } catch (error) {
+    const axiosError = error as AxiosError<ApiErrorResponse>;
+    console.log(error);
+    if (axiosError?.response?.data?.message) {
+      console.log(axiosError?.response?.data?.message);
+
+      toast.error(axiosError?.response?.data?.message);
+    } else {
+      toast.error("An unexpected error occurred. Please try again later.");
+    }
+    return null;
+  } finally {
+    if (setLoading) setLoading(false);
+  }
+};
